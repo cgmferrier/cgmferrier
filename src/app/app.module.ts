d634cd32d07118392a6f52b27a14ea14b866ffdd
Cecile Ferrier <cecile.ferrier@runpath.com>
2020-02-12 13:56:50 +0000

    Generated skill-chart component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ChartsModule } from 'ng2-charts';
 import { AppComponent } from './app.component';
 import { IconsModule } from './icons.module';
+import { SkillsChartComponent } from './skills-chart/skills-chart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SkillsChartComponent,
   ],
   imports: [
     BrowserModule,
+    ChartsModule,
     IconsModule,
   ],
   providers: [],
---
+++ src/app/skills-chart/skills-chart.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-skills-chart',
+  templateUrl: './skills-chart.component.html',
+  styles: []
+})
+export class SkillsChartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
